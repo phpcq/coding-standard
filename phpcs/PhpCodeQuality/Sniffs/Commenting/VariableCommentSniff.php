9e6fb7721c3d016d805c0d961cd1ca5810273b6a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-08-12 21:52:29 +0200

    Fix variable comment to also allow deprecated tags.

--- phpcs/PhpCodeQuality/Sniffs/Commenting/VariableCommentSniff.php
+++ phpcs/PhpCodeQuality/Sniffs/Commenting/VariableCommentSniff.php
@@ -37,7 +37,7 @@
  *
  * @SuppressWarnings(PHPMD.CamelCaseClassName)
  */
-class PhpCodeQuality_Sniffs_Commenting_VariableCommentSniff extends Squiz_Sniffs_Commenting_VariableCommentSniff
+class PhpCodeQuality_Sniffs_Commenting_VariableCommentSniff extends PHP_CodeSniffer_Standards_AbstractVariableSniff
 {
     /**
      * Add an error and automatically fix it if desired.
@@ -72,7 +72,9 @@ class PhpCodeQuality_Sniffs_Commenting_VariableCommentSniff extends Squiz_Sniffs
      * Ensure the first line start with capital letter and ends with full stop.
      *
      * @param PHP_CodeSniffer_File $phpcsFile    The file being scanned.
+     *
      * @param int                  $commentStart The position in the stack where the comment started.
+     *
      * @param int                  $commentEnd   The position in the stack where the comment ended.
      *
      * @return void
@@ -131,9 +133,14 @@ class PhpCodeQuality_Sniffs_Commenting_VariableCommentSniff extends Squiz_Sniffs
      * Called to process class member vars.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     *
      * @param int                  $stackPtr  The position of the current token in the stack passed in $tokens.
      *
      * @return void
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
      */
     public function processMemberVar(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
@@ -174,10 +181,110 @@ class PhpCodeQuality_Sniffs_Commenting_VariableCommentSniff extends Squiz_Sniffs
         $previous                        = PHP_CodeSniffer::$allowedTypes;
         PHP_CodeSniffer::$allowedTypes[] = 'int';
         PHP_CodeSniffer::$allowedTypes[] = 'bool';
-        parent::processMemberVar($phpcsFile, $stackPtr);
+
+
+        $foundVar = null;
+        foreach ($tokens[$commentStart]['comment_tags'] as $tag) {
+            if ($tokens[$tag]['content'] === '@var') {
+                if ($foundVar !== null) {
+                    $phpcsFile->addError(
+                        'Only one @var tag is allowed in a member variable comment',
+                        $tag,
+                        'DuplicateVar'
+                    );
+                } else {
+                    $foundVar = $tag;
+                }
+            } elseif ($tokens[$tag]['content'] === '@see') {
+                // Make sure the tag isn't empty.
+                $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $tag, $commentEnd);
+                if ($string === false || $tokens[$string]['line'] !== $tokens[$tag]['line']) {
+                    $error = 'Content missing for @see tag in member variable comment';
+                    $phpcsFile->addError($error, $tag, 'EmptySees');
+                }
+            } elseif ($tokens[$tag]['content'] === '@deprecated') {
+                $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $tag, $commentEnd);
+                if ($string === false || $tokens[$string]['line'] !== $tokens[$tag]['line']) {
+                    $error = 'Content missing for @deprecated tag in member variable comment';
+                    $phpcsFile->addError($error, $tag, 'EmptyDeprecated');
+                }
+            } else {
+                $error = '%s tag is not allowed in member variable comment';
+                $data  = array($tokens[$tag]['content']);
+                $phpcsFile->addWarning($error, $tag, 'TagNotAllowed', $data);
+            }
+        }
+
+        // The @var tag is the only one we require.
+        if ($foundVar === null) {
+            $error = 'Missing @var tag in member variable comment';
+            $phpcsFile->addError($error, $commentEnd, 'MissingVar');
+            return;
+        }
+
+        $firstTag = $tokens[$commentStart]['comment_tags'][0];
+        if ($foundVar !== null && $tokens[$firstTag]['content'] !== '@var') {
+            $error = 'The @var tag must be the first tag in a member variable comment';
+            $phpcsFile->addError($error, $foundVar, 'VarOrder');
+        }
+
+        // Make sure the tag isn't empty and has the correct padding.
+        $string = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $foundVar, $commentEnd);
+        if ($string === false || $tokens[$string]['line'] !== $tokens[$foundVar]['line']) {
+            $error = 'Content missing for @var tag in member variable comment';
+            $phpcsFile->addError($error, $foundVar, 'EmptyVar');
+            return;
+        }
+
+        $varType       = $tokens[($foundVar + 2)]['content'];
+        $suggestedType = PHP_CodeSniffer::suggestType($varType);
+        if ($varType !== $suggestedType) {
+            $error = 'Expected "%s" but found "%s" for @var tag in member variable comment';
+            $data  = array(
+                $suggestedType,
+                $varType,
+            );
+            $phpcsFile->addError($error, ($foundVar + 2), 'IncorrectVarType', $data);
+        }
 
         PHP_CodeSniffer::$allowedTypes = $previous;
 
         $this->checkShortComment($phpcsFile, $commentStart, $commentEnd);
+    }
+
+    /**
+     * Called to process a normal variable.
+     *
+     * Not required for this sniff.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The PHP_CodeSniffer file where this token was found.
+     * @param int                  $stackPtr  The position where the double quoted
+     *                                        string was found.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function processVariable(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+
+    }
+
+    /**
+     * Called to process variables found in double quoted strings.
+     *
+     * Not required for this sniff.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The PHP_CodeSniffer file where this token was found.
+     * @param int                  $stackPtr  The position where the double quoted
+     *                                        string was found.
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function processVariableInString(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+
     }
 }
